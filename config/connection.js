f436f00f4c924450d3c06074105d5fb64c0f5890
Christine Clemens <christineclemens.tesol@gmail.com>
2020-07-20 21:30:36 -0400

    Completed intitial setup for connection.js file.

---
+++ config/connection.js
@@ -0,0 +1,39 @@
+//Define npm dependencies.
+const mysql = require("mysql");
+require("dotev").config();
+
+class Database {
+    constructor( config ) {
+        this.connection = mysql.createConnection( config );
+    }
+    query( sql, args ) {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.query( sql, args, ( err, rows ) => {
+                if ( err )
+                    return reject( err );
+                resolve( rows );
+            } );
+        } );
+    }
+    close() {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.end( err => {
+                if ( err )
+                    return reject( err );
+                resolve();
+            });
+        });
+    }
+}
+
+// Access SQL Database
+const db = new Database({
+    host: "localhost",
+    port: 8080,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
+    insecureAuth : true
+});
+
+module.exports = Database;
