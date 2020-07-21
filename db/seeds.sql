cb4a4740135cb0dc73c433a46df3442f0f8d668d
Christine Clemens <christineclemens.tesol@gmail.com>
2020-07-20 20:22:32 -0400

    Created and populated the burgers_db table in schema and seeds files.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers (id, burger_name, devoured) VALUES (1, "Krabby Patty", true);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (2, "Good Burger", false);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (3, "Bob's Burger", true);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (4, "Chum Burger", false);
