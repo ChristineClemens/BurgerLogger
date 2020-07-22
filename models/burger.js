b41d1da2e939144f76994af7155bd09acb21ca68
Christine Clemens <christineclemens.tesol@gmail.com>
2020-07-21 21:53:24 -0400

    Fixed BurgerModel functions in models/burger.js file.

--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,23 @@
 const orm = require("../config/orm");
 
 class BurgerModel {
-    async addBurger(tableName, value) {
+    async addBurger(burger) {
         const db = new orm();
-        await db.insertOne(tableName, value);
+        await db.insertOne("burgers", burger);
         await db.close();
     }
 
-    async updateBurger(tableName, update, condition) {
+    async updateBurger(burger, condition) {
         const db = new orm();
-        await db.updateOne(tableName, update, condition);
+        await db.updateOne("burgers", burger, condition);
         await db.close();
     }
 
-    async removeBurger(tableName, condition) {
+    async removeBurger(condition) {
         const db = new orm();
-        await db.removeOne(tableName, condition);
+        await db.removeOne("burgers", condition);
         await db.close();
     }
 }
+
+module.exports = BurgerModel;
