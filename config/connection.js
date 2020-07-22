8c7466d92a8b1fb53e04a21050718f74b3b9b77b
Christine Clemens <christineclemens.tesol@gmail.com>
2020-07-21 20:10:30 -0400

    Corrected connection.js file.

--- config/connection.js
+++ config/connection.js
@@ -2,38 +2,26 @@
 const mysql = require("mysql");
 require("dotev").config();
 
-class Database {
-    constructor( config ) {
-        this.connection = mysql.createConnection( config );
-    }
-    query( sql, args ) {
-        return new Promise( ( resolve, reject ) => {
-            this.connection.query( sql, args, ( err, rows ) => {
-                if ( err )
-                    return reject( err );
-                resolve( rows );
-            } );
-        } );
-    }
-    close() {
-        return new Promise( ( resolve, reject ) => {
-            this.connection.end( err => {
-                if ( err )
-                    return reject( err );
-                resolve();
-            });
-        });
-    }
+function connectDB () {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 8080,
+        user: process.env.DB_USER,
+        password: process.env.DB_PWD,
+        database: process.env.DB_NAME,
+        insecureAuth : true
+    });
+    return connection;
 }
 
-// Access SQL Database
-const db = new Database({
-    host: "localhost",
-    port: 8080,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-    insecureAuth : true
-});
+function close(connection) {
+    return new Promise( ( resolve, reject ) => {
+        connection.end( err => {
+            if ( err )
+                return reject( err );
+            resolve();
+        });
+    });
+}
 
-module.exports = Database;
+module.exports = {connectDB, close};
